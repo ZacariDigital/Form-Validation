f28fb88cef920f5e757188cc8da1533d63a8c9cf
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-11-14 16:05:34 +0000

    form validation
    added validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var usernameField = document.querySelector('#usernameField');
+var userNameErrorMessage = document.querySelector('#usernameerrorMessage')
+
+signUpForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    console.log(signUpForm.username.value);
+
+    if(signUpForm.username.value == false){
+        usernameField.classList.add('errorField');
+        userNameErrorMessage.classList.remove('hidden');
+        console.log('You need to enter a username')
+
+    }
+
+
+} );
